2e9de5fc687f0cf621641815770b1e27585360b0
TexasBall! <vishruthgm1@gmail.com>
2024-11-06 17:57:18 -0600

    Add files via upload

---
+++ sqrt.py
@@ -0,0 +1,18 @@
+import math
+oddsr = []
+evensr = []
+reg = []
+start = int(input("Enter starting number"))
+end = int(input("Enter end number"))
+step = 1
+for j in range(start, end + 1, step):
+    reg.append(j)
+print(reg)
+for i in range(start, end + 1, step):
+    sqrt = math.sqrt(i)
+    if int(sqrt) % 2 == 0:
+        evensr.append(sqrt)
+    else:
+        oddsr.append(sqrt)
+print(f"Even square roots are {evensr}")
+print(f"Odd square roots are {oddsr}")    
